46fcb988a2785d5674e005f885c28a630829b150
RomaPress <romapres00@gmail.com>
2020-02-03 00:34:03 +0200

    refactored foreach ad removed redundant assignments

--- src/main/java/com/press/DifferentNumbers.java
+++ src/main/java/com/press/DifferentNumbers.java
@@ -2,27 +2,17 @@ package com.press;
 
 //  56. Дан одномерный массив чисел. Найти количество различных чисел этого массива
 
-public class DifferentNumbers {
+import java.util.Arrays;
 
-    public int differentNumbers(int[] arr) {
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+class DifferentNumbers {
+    int differentNumbers(int[] arr) {
+        Arrays.sort(arr);
         int count = arr.length;
         for (int i = 0; i < arr.length-1; i++) {
             if (arr[i] == arr[i + 1]) {
                 count--;
             }
-
         }
-        System.out.print("Count = " + count);
         return count;
     }
 }
